244e8679f2e3e0d41a5cef27dcf32dbe08413563
manvi08 <57530867+manvi08@users.noreply.github.com>
2020-04-11 12:33:03 +0530

    Final edit

--- gifts.cpp
+++ gifts.cpp
@@ -1,30 +1,10 @@
-//steps which are commented can be used if the program needs to be executed for n no.of students 
 #include<stdio.h>
 struct student{
 	int no_of_gift;
 	int arrt;
 	int id;
 }s[10];
-void sort_arival(/*int n*/){
-	struct student a;
-	int min,pos;
-	for(int i=0;i<10/*n*/;i++){
-		min = s[i].arrt;
-		pos = i;	
-		for(int j=i+1;j<10/*n*/;j++){
-			if(min>s[j].arrt){
-				min = s[j].arrt;
-				pos = j;
-			}
-		}
-		if(i!=pos){
-			a=s[i];
-			s[i]=s[pos];
-			s[pos]=a;
-		}
-	}
-}
-/*void sort_arival(int n){
+void sort_arival(int n){
 	struct student a;
 	int min,pos;
 	for(int i=0;i<n;i++){
@@ -42,26 +22,26 @@ void sort_arival(/*int n*/){
 			s[pos]=a;
 		}
 	}
-}*/
+}
 int main(){
-	/*int n;
+	int n;
 	printf("Enter the no.of students:");
 	scanf("%d",&n);
-	struct student s[n];*/
-	for(int i=0;i<10/*n*/;i++){
+	struct student s[n];
+	for(int i=0;i<n;i++){
 		s[i].id=i+1;
 		printf("\nEnter the arrival time for s%d: ",i+1);
 		scanf("%d",&s[i].arrt);
 		printf("\nEnter the number of gift items for s%d: ",i+1);
 		scanf("%d",&s[i].no_of_gift);
 	}
-	sort_arival(/*n*/);
+	sort_arival(n);
 	struct student m;
 	int pos;
-	for(int i=0;i<10/*n*/;i++){
+	for(int i=0;i<n;i++){
 		m = s[i];
 		pos = i;
-		for(int j=i+1;j<10/*n*/;j++){
+		for(int j=i+1;j<n;j++){
 			if(m.no_of_gift<s[j].no_of_gift){
 				m = s[j];
 				pos = j;
